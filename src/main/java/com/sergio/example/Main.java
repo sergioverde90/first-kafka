8cb9698051b5b13da020729847a7522435e9ec90
Sergio Verde <sergioverde90@gmail.com>
2017-11-26 20:28:09 +0100

    kafka producer example added

--- src/main/java/com/sergio/example/Main.java
+++
@@ -1,9 +0,0 @@
-package com.sergio.example;
-
-public class Main {
-
-    public static void main(String[] args) {
-
-    }
-
-}
---
+++ src/main/java/com/sergio/example/SampleKafkaProducerAsync.java
@@ -0,0 +1,46 @@
+package com.sergio.example;
+
+import org.apache.kafka.clients.producer.*;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+import java.util.Properties;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
+
+public class SampleKafkaProducerAsync {
+
+    public static void main(String[] args) {
+
+        Properties props = new Properties();
+        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "kafka:9092");
+        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        // ACK's config is the most important flag when is important the message lost.
+        // acks='0' => The producer will not wait for response from broker. The producer assume that message has been
+        //             sent successfully
+        // acks='1' => The producer waits until the broker leader has retrieved the message. If error occurred the
+        //             producer can retry sending the message avoiding loss data. The message can still
+        //             be lost if leader has crashed or has not still elected and no replica has the message.
+        //             Can increases the latency.
+        // acks='all' => The producer retrieves the successful response when the leader and all replicas have retrieved
+        //             the message. This is the safest mode. The latency is high in this mode.
+        props.put(ProducerConfig.ACKS_CONFIG, "1"); // wait leader broker to get response
+        Producer<String, String> producer = new KafkaProducer<String, String>(props);
+
+        try {
+            // async fashion
+            System.out.println("sending message...");
+            Future<RecordMetadata> record = producer.send(new ProducerRecord<String, String>("topic1", "key1", "hello world! from Kafka"));
+            System.out.println("message sent!");
+            System.out.println("waiting response from kafka broker...");
+            record.get();
+            System.out.println("message has been retrieved successfully");
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }finally {
+            producer.close();
+        }
+    }
+}
